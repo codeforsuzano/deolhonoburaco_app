97af6ee726a1685e5cc9f62e9046bdba024afaae
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-05-19 09:43:31 -0300

    start 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,14 +10,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from  './auth/auth.module';
+
+// Modal Pages
+import { ImagePageModule } from './pages/modal/image/image.module';
+import { SearchFilterPageModule } from './pages/modal/search-filter/search-filter.module';
+
+// Components
+import { NotificationsComponent } from './components/notifications/notifications.component';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ImagePageModule,
+    SearchFilterPageModule,
+    AuthModule
   ],
   providers: [
     StatusBar,
