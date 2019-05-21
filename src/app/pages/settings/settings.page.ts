582e5eaa2c336fcb9661bfbf9e43dfe6624ff4cf
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-05-21 10:06:15 -0300

    configs

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
@@ -13,14 +14,29 @@ export class SettingsPage implements OnInit {
   currency: any;
   enablePromo: any;
   enableHistory: any;
+  name_user:string = '';
+  email_user:string = '';
 
   languages: any = ['English', 'Portuguese', 'French'];
   paymentMethods: any = ['Paypal', 'Credit Card'];
   currencies: any = ['USD', 'BRL', 'EUR'];
 
-  constructor(public navCtrl: NavController) { }
+  constructor(
+    public navCtrl: NavController,
+    public storage: Storage
+  ) { }
 
   ngOnInit() {
+    this.storage.get('name_user').then(name_user => {
+      console.log(name_user);
+      this.name_user = name_user
+    })
+    this.storage.get('email_user').then(email_user => {
+      console.log(email_user);
+      this.email_user = email_user
+    })
+  
+       
   }
 
   editProfile() {
